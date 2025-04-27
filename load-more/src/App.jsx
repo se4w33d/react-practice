313ca639e9936be3a3b38e15100436d9654dad7f
seaweed <alvinisonline@gmail.com>
2025-04-27 14:23:18 +0800

    fix load more component refreshes whole page every time load more button clicked

--- load-more/src/App.jsx
+++ load-more/src/App.jsx
@@ -3,6 +3,23 @@ import './App.css'
 
 const URL = 'https://dummyjson.com/products?limit=20&'
 
+function ProductList( {products} ) {
+
+  return (
+    <div className='products-container'>
+      {
+        products.map((product) => 
+          <div className='product' key={product.id}>
+            <img src={product.thumbnail} />
+            <p>{product.title}</p>
+          </div>
+        )
+      }
+    </div>
+  )
+}
+
+
 function LoadMoreData( {url} ) {
 
   const [products, setProducts] = useState([]);
@@ -10,25 +27,23 @@ function LoadMoreData( {url} ) {
   const [isLoading, setIsLoading] = useState(false);
   const [isDisabled, setIsDisabled] = useState(false);
 
-
-  async function fetchData(url) {
-    try {
-      setIsLoading(true);
-      const response = await fetch(`${url}skip=${count}`);
-      const json = await response.json();
-      console.log(json);
-      if (count==0) {
-        setProducts(json.products);
-      } else {
-        setProducts([...products, ...json.products])
-      }
-      setIsLoading(false);
-    } catch (error) {
-      console.error(error);
-    }
-  }
-
   useEffect(() => {
+    async function fetchData(url) {
+      try {
+        setIsLoading(true);
+        const response = await fetch(`${url}skip=${count}`);
+        const json = await response.json();
+        console.log(json);
+        if (count==0) {
+          setProducts(json.products);
+        } else {
+          setProducts(prevProducts => [...prevProducts, ...json.products]);
+        }
+        setIsLoading(false);
+      } catch (error) {
+        console.error(error);
+      }
+    }
     if (url) fetchData(url);
   }, [count])
 
@@ -40,28 +55,17 @@ function LoadMoreData( {url} ) {
       }
       return newCount;
     })
-    
-  }
-
-  if (isLoading) {
-    return <div>Loading data...</div>
   }
 
   return (
     <>
-      <div className='products-container'>
-        {
-          products.map((product) => 
-            <div className='product' key={product.id}>
-              <img src={product.thumbnail} />
-              <p>{product.title}</p>
-            </div>
-          )
-        }
-      </div>
+      <ProductList products={products} />
       <div className='btn-container'>
         <button onClick={handleLoadMore} disabled={isDisabled}>Load more product</button>
       </div>
+      {
+        isLoading ? <div>Loading data...</div> : null
+      }
     </>
   )
 }
